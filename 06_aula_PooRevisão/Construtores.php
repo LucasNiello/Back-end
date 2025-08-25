2e125c47b7c9112da24c384128bc371c277fb455
LucasNiello <termminiello.lucas@gmail.com>
2025-08-25 15:46:45 -0300

    06_aula(2)

--- 06_aula_PooRevisão/Construtores.php
+++ 06_aula_PooRevisão/Construtores.php
@@ -35,13 +35,13 @@ AULA 06
         }
 
         //Criando objetos SEM CONSTRUTOR FEITO
-        $produto1 = new Produtos();
+        $produto1 = new ProdutosMercado();
         $produto1->nome = "Suco Tang";
         $produto1->categoria = "Bebidas";
         $produto1->fornecedor = "Mondelez";
         $produto1->qtde_estoque = 200;
 
-        $produto2 = new Produtos();
+        $produto2 = new ProdutosMercado();
         $produto2->nome = "Energético Monster";
         $produto2->categoria = "Bebidas";
         $produto2->fornecedor = "Monster Inc";
