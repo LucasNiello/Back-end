c50a538f3ba3d5fd4c03f0dd0313a442b9141a10
LucasNiello <termminiello.lucas@gmail.com>
2025-09-22 16:31:08 -0300

    # errores

--- 11_aula-Grupo/ATV_3_erros.php
+++ 11_aula-Grupo/ATV_3_erros.php
@@ -1,123 +1,85 @@
-<?php 
-
-// <-- tag de abertura PHP: aparece no seu arquivo, mas NÃO é necessária para Java; remova se quiser um arquivo .java
-// Regras do projeto: // comentário que lista as regras do projeto (somente anotação; não afeta execução)
-// Criar a classe diciplina escolar; // requisito descrito
-// Atributos privasdos, minimo 4; // requisito descrito (atenção: "privasdos" = privados)
-// Configurar encapsulamento; // requisito descrito (usar getters/setters)
-// Minimo 2 metodos; // requisito descrito
-// Criar 3 objetos e testar os metodos para cada um. // requisito descrito
-
-// DESENVOLVER DOCUMETNAÇÃO DO PROJETO // instrução para documentação
-// Descrever todos os metodos e atributos; // instrução
-// Listar objetos; // instrução
-// Descrever caso setter tenha alguma peculiaridade; // instrução
-// Classe DisciplinaEscolar // comentário indicando o que vem a seguir
-
+<?php
+// Jogo 3 Erros Código Php
 class Disciplinas {
-    private $nomeDisciplina;
-    private $cargaHoraria;
-    private $professor;
-    private $periodo;
-    private $notaFinal;
-    private $presenca;
+private $nomeDisciplina; private $cargaHoraria; private $professor; private $periodo;
+private $notaFinal; private $presenca;
 
-    public function __construct($nomeDisciplina, $cargaHoraria, $professor, $periodo, $notaFinal, $presenca) {
-        $this->nomeDisciplina = $nomeDisciplina;
-        $this->cargaHoraria = $cargaHoraria;
-        $this->professor = $professor;
-        $this->periodo = $periodo;
-        $this->notaFinal = $notaFinal;
-        $this->presenca = $presenca;
-    }
+public function 	construct($nomeDisciplina, $cargaHoraria, $professor, $periodo, $notaInicial, $presenca) {
+$this->nomeDisciplina = $nomeDisciplina;
+$this->cargaHoraria = $cargaHoraria;
+$this->professor = $professor;
+$this->periodo = $periodo;
+$this->notaFinal = $notaFinal;
+$this->presenca = $presenca;
+}
 
-    // Métodos de verificação
-    public function aprovado() {
-        return $this->notaFinal >= 6;
-    }
+// Métodos de verificação public function aprovado() {
+return $this->notaFinal >= 6;
+}
 
-    public function presencaValida() {
-        return $this->presenca >= 60;
-    }
+public function presencaValida() { return $this->presenca >= 60;
+}
 
-    public function situacaoFinal() {
-        return ($this->aprovado() && $this->presencaValida()) ? "Aprovado" : "Reprovado";
-    }
+public function situacaoFinal() {
+return ($this->aprovado() && $this->presencaValida()) @ "Aprovado" : "Reprovado";
+}
 
-    // Método resumo
-    public function resumo() {
-        return "Disciplina: " . $this->nomeDisciplina . PHP_EOL .
-               "Carga Horária: " . $this->cargaHoraria . " horas" . PHP_EOL .
-               "Professor: " . $this->professor . PHP_EOL .
-               "Período: " . $this->periodo . PHP_EOL .
-               "Nota Final: " . $this->notaFinal . PHP_EOL .
-               "Presença: " . $this->presenca . "%" . PHP_EOL .
-               "Situação: " . $this->situacaoFinal() . PHP_EOL .
-               "-----------------------------" . PHP_EOL;
-    }
+// Método resumo
+public function resumo() {
+return "Disciplina: " . $this->nomeDisciplina . PHP_EOL .
+"Carga Horária: " . $this->cargaHoraria . " horas" . PHP_EOL .
 
-    // Getters
-    public function getNomeDisciplina() {
-        return $this->nomeDisciplina;
-    }
+"Professor: " . $this->professor . PHP_EOL . "Período: " . $this->periodo . PHP_EOL . "Nota Final: " . $this->notaFinal . PHP_EOL .
+"Presença: " . $this->presenca . "%" . PHP_EOL . "Situação: " . $this->situacaoFinal() . PHP_EOL . "	" . PHP_EOL;
+}
 
-    public function getCargaHoraria() {
-        return $this->cargaHoraria;
-    }
+// Getters
+public function getNomeDisciplina() { return $this->nomeDisciplina;
+}
 
-    public function getProfessor() {
-        return $this->professor;
-    }
+public function getCargaHoraria() { return $this->cargaHoraria;
+}
 
-    public function getPeriodo() {
-        return $this->periodo;
-    }
+public function getProfessor() { return $this->professor;
+}
 
-    public function getNotaFinal() {
-        return $this->notaFinal;
-    }
+public function getPeriodo() { return $this->periodo;
+}
 
-    public function getPresenca() {
-        return $this->presenca;
-    }
+public function getNotaFinal() { return $this->notaFinal;
+}
 
-    // Setters
-    public function setNomeDisciplina($nomeDisciplina) {
-        $this->nomeDisciplina = $nomeDisciplina;
-    }
+public function getPresenca() { return $this->presenca;
+}
 
-    public function setCargaHoraria($cargaHoraria) {
-        $this->cargaHoraria = $cargaHoraria;
-    }
+// Setters
+public function setNomeDisciplina($nomeDisciplina) {
+$this->nomeDisciplina = $nomeDisciplina;
+}
 
-    public function setProfessor($professor) {
-        $this->professor = $professor;
-    }
+public function setCargaHoraria($cargaHoraria) {
+$this->cargaHoraria = $cargaHoraria;
+}
 
-    public function setPeriodo($periodo) {
-        $this->periodo = $periodo;
-    }
+public function setProfessor($professor) {
+$this->professor = $professor;
+}
 
-    public function setNotaFinal($notaFinal) {
-        $this->notaFinal = $notaFinal;
-    }
+public function setNotaFinal($notaFinal) {
+$this->notaFinal = $notaFinal;
+}
 
-    public function setPresenca($presenca) {
-        $this->presenca = $presenca;
-    }
+public function setPresenca($presenca) {
+$this->presenca = $presenca;
+}
 }
 
 // Testando
 $d1 = new Disciplinas("PHP", 80, "Samuka", "Noturno", 8.5, 75);
 $d2 = new Disciplinas("JavaScript", 60, "Brunão", "Matutino", 5.0, 55);
-$d3 = new Disciplinas("HTML e CSS", 40, "Bruno", "Vespertino", 7.0, 65);
+$d3 = new Disciplinas("HTML e CSS", 40, "Cleiton", "Vespertino", 7.0, 65);
 
-echo $d1->resumo();
-echo $d2->resumo();
-echo $d3->resumo();
+echo $d1->resumo(); echo $d2->resumo(); echo $d3->resumo();
 
-$d2->setNotaFinal(6.5);
-echo $d2->resumo();
-
-
+$d2->setNotaFinal(6.5); echo $d2->resumo();
 ?>
